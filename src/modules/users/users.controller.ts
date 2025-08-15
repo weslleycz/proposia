5310fd8647aeb89dd2e1da431aac9db952548b76
weslleycz <weslleysouza22@gmail.com>
2025-08-15 19:42:42 -0300

    ✨ feat: adiciona funcionalidades de listagem e busca de usuários no módulo de usuários, incluindo tratamento de erros e documentação Swagger

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserEntity } from './entities/user.entity';
 
 @ApiTags('Users')
 @Controller('users')
@@ -15,5 +16,28 @@ export class UsersController {
   @ApiResponse({ status: 400, description: 'Requisição inválida.' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Lista todos os usuários' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'A lista de usuários foi retornada com sucesso.',
+    type: [UserEntity],
+  })
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Busca um usuário pelo ID' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'O usuário foi retornado com sucesso.',
+    type: UserEntity,
+  })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado.' })
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/common/services';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -33,4 +33,39 @@ export class UsersService {
     const { passwordHash: _, ...result } = user;
     return result;
   }
+
+  async findAll() {
+    const users = await this.prismaService.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    return users;
+  }
+
+  async findOne(id: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`Usuário com ID "${id}" não encontrado`);
+    }
+
+    return user;
+  }
 }
+
